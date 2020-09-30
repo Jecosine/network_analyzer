0f8dbf7cdb847689d70d0c01bdf9b8bf12200679
JecosineMoc <mj_ol@outlook.com>
2020-09-30 09:02:16 +0800

    INIT

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'network_snipper'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
