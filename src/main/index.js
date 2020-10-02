dc4ecca896919f9172e8422692a8df101ea3a485
Jecosine <mj_ol@outlook.com>
2020-10-03 02:27:10 +0800

    UPDATE:Finish basic package analyzing

--- src/main/index.js
+++ src/main/index.js
@@ -3,23 +3,25 @@
  * @LastEditors: Jecosine
  * @LastEditTime: 2020-09-30 16:28:15
  */
-import { app, BrowserWindow, Menu } from 'electron'
+import { app, BrowserWindow, Menu } from "electron";
 
 /**
  * Set `__static` path to static files in production
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
  */
-if (process.env.NODE_ENV !== 'development') {
-  global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
+if (process.env.NODE_ENV !== "development") {
+  global.__static = require("path")
+    .join(__dirname, "/static")
+    .replace(/\\/g, "\\\\");
 }
 
-let mainWindow
-const winURL = process.env.NODE_ENV === 'development'
-  ? `http://localhost:9080`
-  : `file://${__dirname}/index.html`
+let mainWindow;
+const winURL =
+  process.env.NODE_ENV === "development"
+    ? `http://localhost:9080`
+    : `file://${__dirname}/index.html`;
 
-
-function createWindow () {
+function createWindow() {
   /**
    * Initial window options
    */
@@ -28,29 +30,29 @@ function createWindow () {
     useContentSize: true,
     width: 1000,
     // remove native navigation bar
-    frame: false
-  })
+    frame: false,
+  });
 
-  mainWindow.loadURL(winURL)
+  mainWindow.loadURL(winURL);
 
-  mainWindow.on('closed', () => {
-    mainWindow = null
-  })
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
 }
 
-app.on('ready', createWindow)
+app.on("ready", createWindow);
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
   }
-})
+});
 
-app.on('activate', () => {
+app.on("activate", () => {
   if (mainWindow === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
 
 /**
  * Auto Updater
